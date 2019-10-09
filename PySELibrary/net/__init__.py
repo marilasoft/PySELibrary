7e714b146852495076d6b99dce8578e79cc03223
marilasoft <marilasoft@marila.sa>
2019-10-09 14:55:41 -0400

    Añadida la opcion de ignorar errores de certificados.

--- PySELibrary/net/__init__.py
+++ PySELibrary/net/__init__.py
@@ -1,25 +1,29 @@
 import requests
 from requests.exceptions import ConnectionError
 
-from PySELibrary import headers
+from PySELibrary import headers as headers_
 from PySELibrary.core import ConnectionException
 
 
-def connect(url, data=None, cookies=None, method="GET"):
+def connect(url, data=None, cookies=None, verify=None, headers=None, method="GET"):
     request = None
     try:
         if data is None:
             data = {}
+        if headers is None:
+            headers = headers_
         if method == "GET":
             request = requests.get(url,
                                    data,
                                    headers=headers,
-                                   cookies=cookies)
+                                   cookies=cookies,
+                                   verify=verify)
         elif method == "POST":
             request = requests.post(url,
                                     data,
                                     headers=headers,
-                                    cookies=cookies)
+                                    cookies=cookies,
+                                    verify=verify)
         return request
     except ConnectionError:
         raise ConnectionException
@@ -30,4 +34,4 @@ def get_cookies(url):
 
 
 def get_captcha(cookies):
-    return requests.get("https://www.portal.nauta.cu/captcha/?", headers=headers, cookies=cookies).content
+    return requests.get("https://www.portal.nauta.cu/captcha/?", headers=headers_, cookies=cookies).content
